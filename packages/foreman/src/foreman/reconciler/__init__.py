7dec5eb47cde569c4264caebd0ae3e9c66d0a07b
wrenrichley <wrenrichley@gmail.com>
2026-06-03 19:10:57 -0400

    feat(reconciler): add GraphQL observer with typed failure modes

--- packages/foreman/src/foreman/reconciler/__init__.py
+++ packages/foreman/src/foreman/reconciler/__init__.py
@@ -3,6 +3,13 @@
 from foreman.reconciler.actions import Action, ActionContext, execute_action
 from foreman.reconciler.exec_log import ExecutionLog
 from foreman.reconciler.host import ReconcilerHost
+from foreman.reconciler.observer import (
+    GHGraphQLClient,
+    ObserverError,
+    ObserverRateLimited,
+    ObserverUnreachable,
+    fetch_project_state,
+)
 from foreman.reconciler.rules import RULES, PrecedenceTier, Rule, evaluate
 from foreman.reconciler.state import IssueState, PRState, ProjectSnapshot
 
@@ -10,7 +17,11 @@ __all__ = [
     "Action",
     "ActionContext",
     "ExecutionLog",
+    "GHGraphQLClient",
     "IssueState",
+    "ObserverError",
+    "ObserverRateLimited",
+    "ObserverUnreachable",
     "PRState",
     "PrecedenceTier",
     "ProjectSnapshot",
@@ -19,4 +30,5 @@ __all__ = [
     "Rule",
     "evaluate",
     "execute_action",
+    "fetch_project_state",
 ]
